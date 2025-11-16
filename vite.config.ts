4d1a60b493cc74f6d99d5fa9eee7edac746a4733
ilvmey <ilvmey@yahoo.com.tw>
2025-11-16 16:01:06 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "hitachi-yungtai",
-  plugins: [react()],
+  base: "error_code",
+  // plugins: [react()],
 })
